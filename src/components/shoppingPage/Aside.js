5adf9828228674b4700cf567ca5b29457b469bd5
Roland Foucher <roland.foucher.pro@gmail.com>
2021-12-03 23:30:13 +0100

    add filtre

--- src/components/shoppingPage/Aside.js
+++ src/components/shoppingPage/Aside.js
@@ -2,7 +2,18 @@ import '../../styles/ShoppingPageStyle/Aside.css'
 
 
 function Aside(){
-    return <div></div>
+    return <aside className="acf-aside">
+        <p>Filtrer les articles</p>
+        <div className="acf-filtre-selector">
+            <select name="typeOfItem"  id= "typeOfItem">
+            
+                <option value="all">tous les articles</option>
+                <option value="sac">Les sacs</option>
+                <option value="cartable">Les sacoches et cartables</option>
+            </select>
+        </div>
+
+    </aside>
 }
 
 export default Aside;
