1a9a149f470a2c07acbb139e1959a796a22277a0
Roland Foucher <roland.foucher.pro@gmail.com>
2021-12-03 20:59:49 +0100

    add app.css

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,7 @@
 import Banner from './Banner'
-<<<<<<< Updated upstream
-=======
 import Nav from './Nav'
 import Footer from './Footer'
 import '../styles' 
->>>>>>> Stashed changes
 
 function App() {
   return (
