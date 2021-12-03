cd66cdce936398045830d8638e3f4ac6950d4507
Roland Foucher <roland.foucher.pro@gmail.com>
2021-12-03 13:41:05 +0100

    init banner

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import Banner from './Banner'
+
+function App() {
+  return (
+    <div>
+      <Banner>
+      </Banner>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Banner.js
@@ -0,0 +1,8 @@
+import logo from '../assets/sac_michele_icone.jpeg'
+import '../styles/Banner.css'
+
+function Banner(){
+    return
+    
+}
+export default Banner;
