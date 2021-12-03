30dd457ac9e095a1ebd96f5f440d853d52d0901d
Roland Foucher <roland.foucher.pro@gmail.com>
2021-12-03 20:55:41 +0100

    change link css to global page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,10 +5,10 @@ function Footer(){
         <footer className="acf-footer">
             <ul className="acf-footer-list">  
                 <li className="acf-footer-list-item"><a href="#">Mention Legal</a></li>
-                <li className="acf-footer-list-item"> <a href="#">contacts</a></li>   
+                <li className="acf-footer-list-item"> <a href="#">Contacts</a></li>   
             </ul>
             <ul className="acf-footer-list acf-footer-admin">  
-                <li className="acf-footer-list-item"><a href="#">admin</a></li>
+                <li className="acf-footer-list-item"><a href="#">Admin</a></li>
                 
             </ul>
             
