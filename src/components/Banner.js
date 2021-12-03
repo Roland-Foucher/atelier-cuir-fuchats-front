1f24a3a10c05d27da7e1d9fe6d574639d853edae
Roland Foucher <roland.foucher.pro@gmail.com>
2021-12-03 13:47:50 +0100

    add img and title + clean init

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,7 +2,12 @@ import logo from '../assets/sac_michele_icone.jpeg'
 import '../styles/Banner.css'
 
 function Banner(){
-    return
+    return( 
+        <header className="acf-header">
+            <img src={logo} alt="icone sac cuir" className = 'acf-logo'/>
+            <h1 className = 'acf-titre'>Atelier Cuir Les Fuchats</h1>
+        </header>
+        )
     
 }
 export default Banner;
