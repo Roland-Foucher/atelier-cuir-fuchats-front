25b563478d85052dae31c8d751f6589a2b4f0e72
Roland Foucher <roland.foucher.pro@gmail.com>
2021-12-22 16:56:55 +0100

    add link footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,17 @@
+import { Link } from "react-router-dom";
 import '../styles/Footer.css'
+
+
 
 function Footer(){
     return(
         <footer className="acf-footer">
             <ul className="acf-footer-list">  
-                <li className="acf-footer-list-item"><a href="#">Mention Legal</a></li>
-                <li className="acf-footer-list-item"> <a href="#">Contacts</a></li>   
+                <li className="acf-footer-list-item"><Link to="mention-legal">Mention Legal</Link></li>
+                <li className="acf-footer-list-item"> <Link to="contact">Contacts</Link></li>   
             </ul>
             <ul className="acf-footer-list acf-footer-admin">  
-                <li className="acf-footer-list-item"><a href="#">Admin</a></li>
+                <li className="acf-footer-list-item"><Link to="admin">Admin</Link></li>
                 
             </ul>
             
