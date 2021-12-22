6a0ed72fbf4f5c433d67ca5072e8959b4ef9a23e
Roland Foucher <roland.foucher.pro@gmail.com>
2021-12-22 16:13:24 +0100

    init wotkshop

--- src/components/workshopPage/Workshop.js
+++ src/components/workshopPage/Workshop.js
@@ -1,6 +1,16 @@
+import maison from '../../assets/workshop/atelier-cuir-fuchats-raisin-sous-balcon.jpeg'
+import maconnais from '../../assets/workshop/atelier-cuir-fuchats-roche-de-vergisson3.jpeg'
+import '../../styles/Workshop/Workshop.css'
+
 function Workshop(){
-    return <main>
-        <h2>workshop</h2>
+    return <main className='acf-workshop-page'>
+        <h2>Bienvenus à l'atelier artisanal de maroquinerie des Fuchats à Bussières (Saône et Loire)</h2>
+        <div className='acf-workshop-images-bussiere'>
+        <img src={maison} alt="maison fushiats" className='acf-workshop-images-bussiere-single' />
+        <img src={maconnais} alt="maison fushiats" className='acf-workshop-images-bussiere-single' />
+        </div>
+        
+        <h3>L'atelier des Fuchats se situe  à Bussières, un village de Bourgogne en Saône et Loire, à 10 kms à l'ouest de Macon.</h3>
     </main>
 }
 
